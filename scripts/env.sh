103720bccc8cb1950a320aace726b007af76d558
Novemser <novemser@gmail.com>
2024-04-28 17:28:56 +0800

    Add env script

---
+++ scripts/env.sh
@@ -0,0 +1,3 @@
+# Remember to use the transformers source code in this repository, instead of the conda/pip installed ones in site-packages.
+# ATTENTION: please execute this script in the project root directory!
+export PYTHONPATH=${PYTHONPATH}:`pwd`/transformers/src
